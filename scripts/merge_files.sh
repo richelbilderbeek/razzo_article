74a9172abc010392da60f4d18bb3678a602a440d
richelbilderbeek <richel@richelbilderbeek.nl>
2020-03-31 08:44:31 +0200

    Can merge files

--- scripts/merge_files.sh
+++ scripts/merge_files.sh
@@ -8,4 +8,14 @@
 #   ./scripts/merge_files.sh
 #
 #
+# 
+# Follow same structure as article.tex
+
+cat razzo-abstract.tex \
+  razzo-introduction.tex \
+  razzo-hypotheses.tex \
+  razzo-methods.tex \
+  razzo-results.tex \
+  fig_results.tex \
+  razzo-discussion.tex > razzo_article.tex
 
